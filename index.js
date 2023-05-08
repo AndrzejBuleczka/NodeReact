41ec51fbb0444174744129e8e2df76de32a3a163
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-08 21:55:56 +0200

    feat: add model class

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./services/passport");
+require('./models/User')
 
-main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect(keys.mongoURI);
-}
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
