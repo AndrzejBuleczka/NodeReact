6f7a6b7d743edc841a883f465cc702975a6fe593
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-08 21:46:38 +0200

    feat: add mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
-require('./services/passport');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(keys.mongoURI);
+}
 
 const app = express();
 
@@ -7,7 +15,7 @@ app.get("/", (req, res) => {
   res.send({ hi: "again and again :D" });
 });
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
