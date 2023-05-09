42c04dca933c31acb241c3173f40c5794b765671
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-09 21:56:53 +0200

    feat: change in config

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID "1045725817764-udjeb9gqq5lkuba8umujml9mhrqa54dr.apps.googleusercontent.com",
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET "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",
+  mongoURI: process.env.MONGO_URI "mongodb://127.0.0.1:27017/emailyprod",
+  cookieKey: process.env.COOKIE_KEY "nldfsjngoerngl;enggr5t945tjgmEEAAęęąąkrngow094u2547WQ%$^%^",
+}
