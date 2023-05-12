92d25dcb6a00d19bc014644d5d3063662c159c63
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-12 21:23:24 +0200

    feat: add authReducer basics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import App from "./App.jsx";
-import "./index.css";
 
 const store = createStore(() => [], applyMiddleware());
 
