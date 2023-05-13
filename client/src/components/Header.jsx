e73bb959594dfcbe69a0a233806d71157bf80c3b
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-13 22:20:59 +0200

    style: add styles to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,18 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <nav>
+    <div className="nav-wrapper">
+      <a className="left brand-logo">Emaily</a>
+      <ul className="right">
+        <li>
+          <a>
+            Login with Google
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
 };
 
 export default Header;
