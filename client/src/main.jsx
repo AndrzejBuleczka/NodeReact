bb10123e4e195d89ce2166a809ade64d1e11bbf3
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-13 22:16:34 +0200

    style: add materialize css lib

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import App from "./App.jsx";
+import "materialize-css/dist/css/materialize.min.css";
 import reducers from "./reducers/index.jsx";
 
 const store = createStore(reducers, {}, applyMiddleware());
