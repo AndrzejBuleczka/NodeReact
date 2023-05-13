7a636fc49ec39657b556912579705caab32c1441
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-13 13:42:53 +0200

    feat: add route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/header" element={<Header />} />
+      <Route path="/dash" element={<Dashboard />} />
+      <Route path="/survey" element={<SurveyNew />} />
+      <Route path="/" element={<Landing />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <a href="/auth/google">Sign in with google</a>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
