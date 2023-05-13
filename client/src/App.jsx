446d64ca6061663c71e15d7df289da490481f98b
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-13 21:06:49 +0200

    Feat: fix router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,22 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/header" element={<Header />} />
-      <Route path="/dash" element={<Dashboard />} />
-      <Route path="/survey" element={<SurveyNew />} />
-      <Route path="/" element={<Landing />} />
-    </Route>
-  )
-);
-
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/surveys" element={<Dashboard />} />
+        <Route path="/surveys/new" element={<SurveyNew />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
