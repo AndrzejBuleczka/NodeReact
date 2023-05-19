3c61e0057d84ac824fa55a628acbdf30d134ed4e
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-05-19 23:01:34 +0200

    add landing component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "./actions";
 
 import Header from "./components/Header";
+import Landing from "./components/Landing";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
