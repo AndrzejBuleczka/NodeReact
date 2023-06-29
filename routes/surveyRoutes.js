f5fae8d085d940ba2e05de5e8014a6fc7e8ed6a2
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-06-29 22:40:47 +0200

    add surveytemplate

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -20,6 +21,6 @@ module.exports = (app) => {
       dateSent: Date.now(),
     });
 
-    const mailer = new Mailer(survey, template);
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
