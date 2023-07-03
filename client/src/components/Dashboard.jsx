840f405ecf5063363394610c4de816ed9fa03e37
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-07-03 21:55:08 +0200

    add material icon add

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
   return (
-    <div>Dashobaord</div>
-  )
+    <div>
+      Dashboard
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Dashobaord;
+
+export default Dashboard;
