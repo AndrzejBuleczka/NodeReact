3c252755731f3129bd92cf9eac2e8a9320c58436
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-07-03 21:20:08 +0200

    add survey templater

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,8 @@ import App from "./App.jsx";
 import "materialize-css/dist/css/materialize.min.css";
 import reducers from "./reducers/index.jsx";
 import reduxThunk from "redux-thunk";
-
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -18,4 +19,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-console.log('Environment is', process.env.NODE_ENV);
+console.log("Environment is", process.env.NODE_ENV);
