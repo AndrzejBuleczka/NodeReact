69fc2071691e25bbe384fa6e3e6360a34f183ec4
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-07-11 14:21:09 +0200

    connect redux to react

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
@@ -12,6 +13,5 @@ const Dashboard = () => {
     </div>
   );
 };
-
 
 export default Dashboard;
